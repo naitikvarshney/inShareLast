3f909689eca6967ad085575d3453ba7a377326dd
Naitik Varshney <naitikvarshney77@gmail.com>
2022-02-10 23:27:37 +0530

    initial

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ app.use(express.static('public'));
 app.use(express.static(__dirname));
 app.use(express.json());
 
+const cors = require('cors');
+// Cors
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
+
 const connectDB = require('./config/db');
 connectDB();
+
+app.use(cors(corsOptions));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
